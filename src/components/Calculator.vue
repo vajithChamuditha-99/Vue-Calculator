f6be063983e28bc6d806483929b4adc6d5c5f20f
Vajith Chamuditha <vajithc@gmail.com>
2021-05-19 10:19:31 +0530

    UI done

---
+++ src/components/Calculator.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn dot">C</div>
+    <div @click="sign" class="btn dot">+/-</div>
+    <div @click="percent" class="btn dot">%</div>
+    <div class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div class="btn operator">×</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div @click="dot" class="btn dot">.</div>
+    <div @click="append('0')" class="btn">0</div>
+    <div class="btn operator equal">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      current: ''
+    }
+  },
+  methods:{
+    clear(){
+      this.current=''
+    },
+    sign(){
+      this.current= this.current.charAt(0) ==='-' ?
+        this.current.slice(1) : `-${this.current}`;
+    },
+    percent(){
+      this.current= `${parseFloat(this.current) / 100}`
+    },
+    append(number){
+      this.current=`${this.current}${number}`;
+    },
+    dot(){
+      if(this.current.indexOf('.')=== -1){
+        this.append('.');
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .calculator{
+    align-content: center;
+    justify-self: center;
+    padding: 0 450px;
+    width: 450px;
+    height: 550px;
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
+  .display{
+    grid-column: 1/5 ;
+    text-align: right;
+    padding: 45px 0;
+    height: 75px;
+    background-color:lightseagreen;
+    color: white;
+    border-radius: 10px;
+  }
+  .equal{
+    grid-column: 3/5;
+  }
+  .btn{
+    background-color:cyan;
+    border: 1px solid #fff;
+    padding: 30px 0;
+  }
+  .operator{
+    background-color:steelblue;
+    color: white ;
+  }
+  .dot{
+    background-color:aquamarine;
+  }
+</style>
